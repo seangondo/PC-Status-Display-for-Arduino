7f4d0feebcad34dab035e91bfb5306d7505daa6d
seang <seang@DESKTOP-L6U8JU3>
2022-04-11 20:38:36 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ImportDLLfiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CPU Temperature";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "GPU Temperature";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 4800;
+            this.serialPort1.Parity = System.IO.Ports.Parity.Odd;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(509, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(509, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(602, 46);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(156, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "Select port...";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "CPU Usage";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "GPU Usage";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(506, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Serial Not Sending Any Message";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(21, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 164);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CPU Info";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(9, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(59, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "CPU Clock";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 28);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "CPU Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Location = new System.Drawing.Point(251, 26);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(224, 164);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "CPU Info";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(10, 138);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(67, 13);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Memory Max";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(10, 113);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(72, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Memory Used";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 28);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "GPU Name";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(21, 202);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(61, 13);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Send Serial";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(602, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Refresh COM";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(781, 227);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Read Temperature for CPU and GPU";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private Button button1;
+        private Button button2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private GroupBox groupBox1;
+        private Label label6;
+        private GroupBox groupBox2;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+using OpenHardwareMonitor.Hardware;
+using System.IO.Ports;
+using System.Globalization;
+using Timer = System.Windows.Forms.Timer;
+using System.Threading;
+
+namespace ImportDLLfiles
+{
+    public partial class Form1 : Form
+    {
+        bool serialSend = false;
+        String portUse = "";
+        String portNameUse = "";
+        String CPU1 = "";
+        String CPU2 = "";
+        String CPU3 = "";
+        String CPU4 = "";
+
+        String GPU1 = "";
+        String GPU2 = "";
+        String GPU3 = "";
+        String GPU4 = "";
+        String GPU5 = "";
+
+        UpdateVisitor updateVisitor = new UpdateVisitor();
+        Computer thisComputer;
+        public Form1()
+        {
+            InitializeTimer();
+            InitializeComponent();
+            thisComputer = new Computer();
+
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+                var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+                foreach (string s in portList)
+                {
+                    comboBox1.Items.Add(s.Substring(7));
+                }
+            }
+
+            //string[] ports = SerialPort.GetPortNames();
+            /*foreach (string port in ports)
+            {
+                comboBox1.Items.Add(port);
+                portUse = port;
+            }*/
+        }
+        public class UpdateVisitor : IVisitor
+        {
+            public void VisitComputer(IComputer computer)
+            {
+                computer.Traverse(this);
+            }
+            public void VisitHardware(IHardware hardware)
+            {
+                hardware.Update();
+                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+            }
+            public void VisitSensor(ISensor sensor) { }
+            public void VisitParameter(IParameter parameter) { }
+        }
+
+        private void InitializeTimer()
+        {
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Interval = 1000;
+            timer1.Enabled = true;
+            timer1.Start();
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            mainProgram();
+            if(serialPort1.IsOpen)
+            {
+                 serialPort1.WriteLine(CPU4 + CPU2 + CPU1 + CPU3);
+                 //Thread.Sleep(100);
+                 serialPort1.WriteLine(GPU5 + GPU2 + GPU1 + GPU3 + GPU4);
+                /*serialPort1.WriteLine(CPU4);
+                Thread.Sleep(100);
+                serialPort1.WriteLine(CPU2);
+                Thread.Sleep(100);
+                serialPort1.WriteLine(CPU1);
+                Thread.Sleep(100);
+                serialPort1.WriteLine(CPU3);
+                Thread.Sleep(100);
+                serialPort1.WriteLine(GPU5);
+                Thread.Sleep(100);
+                serialPort1.WriteLine(GPU2);
+                Thread.Sleep(100);
+                serialPort1.WriteLine(GPU1);
+                Thread.Sleep(100);
+                serialPort1.WriteLine(GPU3);
+                Thread.Sleep(100);
+                serialPort1.WriteLine(GPU4);*/
+                label11.Text = CPU4 + CPU2 + CPU1 + CPU3 + GPU5 + GPU2 + GPU1 + GPU3 + GPU4;
+                //serialPort1.WriteLine(GPU5 + GPU2 + GPU1 + GPU3 + GPU4);
+            }
+        }
+
+        private void mainProgram()
+        {
+            thisComputer.Open();
+            thisComputer.CPUEnabled = true;
+            thisComputer.GPUEnabled = true;
+            thisComputer.Accept(updateVisitor);
+            for (int i = 0; i < thisComputer.Hardware.Length; i++)
+            {
+                if (thisComputer.Hardware[i].HardwareType == HardwareType.CPU)
+                {
+                    for (int j = 0; j < thisComputer.Hardware[i].Sensors.Length; j++)
+                    {
+                        if (thisComputer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
+                        {
+                            if (thisComputer.Hardware[i].Sensors[j].Name == "CPU Package")
+                            {
+                                float cpuTemp = (float)thisComputer.Hardware[i].Sensors[j].Value;
+                                if (serialSend == true)
+                                {
+                                    CPU1 = Math.Round(cpuTemp, 2, MidpointRounding.ToEven).ToString() + "CPU;";
+                                    CPU4 = thisComputer.Hardware[i].Name + "CAB;";
+                                    //serialPort1.WriteLine(thisComputer.Hardware[i].Name + "CNM");
+                                }
+                                label6.Text = thisComputer.Hardware[i].Name;
+                                label1.Text = thisComputer.Hardware[i].Sensors[j].Name + "\t : \t" + Math.Round(cpuTemp, 2, MidpointRounding.ToEven).ToString() + "\r";
+                            }
+                        }
+                        if (thisComputer.Hardware[i].Sensors[j].SensorType == SensorType.Load)
+                        {
+                            CPU2 = Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + "UCP;";
+                            label3.Text = thisComputer.Hardware[i].Sensors[j].Name + "\t : \t" + Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + " %";
+                        }
+                        if (thisComputer.Hardware[i].Sensors[j].SensorType == SensorType.Clock)
+                        {
+                            CPU3 = Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + "CCP;";
+                            label8.Text = thisComputer.Hardware[i].Sensors[j].Name + "\t : \t" + Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 0, MidpointRounding.ToEven).ToString() + " MHz";
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < thisComputer.Hardware.Length; i++)
+            {
+                if (thisComputer.Hardware[i].HardwareType == HardwareType.GpuNvidia)
+                {
+                    for (int j = 0; j < thisComputer.Hardware[i].Sensors.Length; j++)
+                    {
+                        if (thisComputer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
+                        {
+                            if (serialSend == true)
+                            {
+                                GPU1 = thisComputer.Hardware[i].Sensors[j].Value.ToString() + "GPU;";
+                                GPU5 = thisComputer.Hardware[i].Name.Substring(22) + "GNM;";
+                                //serialPort1.WriteLine(thisComputer.Hardware[i].Name.Substring(22) + "GNM");
+                            }
+                            label7.Text = thisComputer.Hardware[i].Name.Substring(7);
+                            label2.Text = thisComputer.Hardware[i].Sensors[j].Name + "\t : \t" + thisComputer.Hardware[i].Sensors[j].Value.ToString() + "\r";
+                        }
+                        if (thisComputer.Hardware[i].Sensors[j].SensorType == SensorType.Load)
+                        {
+                            if (thisComputer.Hardware[i].Sensors[j].Name == "GPU Core")
+                            {
+                                GPU2 = Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + "UGP;";
+                                label4.Text = thisComputer.Hardware[i].Sensors[j].Name + "\t : \t" + Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + " %";
+                            }
+                        }
+                        if (thisComputer.Hardware[i].Sensors[j].SensorType == SensorType.SmallData)
+                        {
+                            if (thisComputer.Hardware[i].Sensors[j].Name == "GPU Memory Used")
+                            {
+                                GPU3 = Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + "GUS;";
+                                label9.Text = thisComputer.Hardware[i].Sensors[j].Name + "\t : \t" + Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + " MB";
+                            }
+                            if (thisComputer.Hardware[i].Sensors[j].Name == "GPU Memory Total")
+                            {
+                                GPU4 = Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + "GMX;";
+                                label10.Text = thisComputer.Hardware[i].Sensors[j].Name + "\t : \t" + Math.Round((float)thisComputer.Hardware[i].Sensors[j].Value, 2, MidpointRounding.ToEven).ToString() + " MB";
+                            }
+                        }
+                    }
+                }
+            }
+            thisComputer.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                if(portNameUse != "")
+                {
+                    serialSend = true;
+                    label5.Text = "Serial Working on Port : " + portNameUse;
+                    serialPort1.PortName = portNameUse;
+                    serialPort1.Open();
+                }
+                else
+                {
+                    label5.Text = "Please select port first!";
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(serialPort1.IsOpen)
+            {
+                serialSend = false;
+                label5.Text = "Serial Not Sending Any Message";
+                //serialPort1.WriteLine("CPUGPU\n");
+                serialPort1.Close();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String selectedPort = comboBox1.SelectedItem.ToString();
+            portNameUse = selectedPort.Substring(selectedPort.Length - 5, 4);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            comboBox1.Text = "Select port...";
+            using (var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_PnPEntity WHERE Caption like '%(COM%'"))
+            {
+                var portnames = SerialPort.GetPortNames();
+                var ports = searcher.Get().Cast<ManagementBaseObject>().ToList().Select(p => p["Caption"].ToString());
+                var portList = portnames.Select(n => n + " - " + ports.FirstOrDefault(s => s.Contains(n))).ToList();
+
+                foreach (string s in portList)
+                {
+                    comboBox1.Items.Add(s.Substring(7));
+                }
+            }
+        }
+    }
+}
